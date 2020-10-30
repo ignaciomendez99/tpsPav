7a31eb2bc11a5f3c2b586443b8aeaad235c3f756
Lorenzo Sala <salalorennn@gmail.com>
2020-10-30 01:23:40 -0300

    Añadimos los reportes al  sistema,ñ.

--- GUI/MainForm.cs
+++ GUI/MainForm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TPS_PAV.Entities;
 using TPS_PAV.GUI;
+using TPS_PAV.GUI.Reports;
 
 namespace TPS_PAV
 {
@@ -60,6 +61,12 @@ namespace TPS_PAV
         private void ayudaToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Grupo Nro: 3 - PAVI 2020\nSala, Lorenzo - Mendez, Ignacio - Zago, Agustín - Otero, Gastón", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void avancePorCursoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormReporte rf = new FormReporte();
+            rf.ShowDialog();
         }
     }
 }
---
+++ GUI/Reports/FormReporte.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TPS_PAV.GUI.Reports
+{
+    partial class FormReporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.DataSetReportes = new TPS_PAV.GUI.Reports.DataSetReportes();
+            this.DataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataTable1TableAdapter = new TPS_PAV.GUI.Reports.DataSetReportesTableAdapters.DataTable1TableAdapter();
+            this.DataTable2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataTable2TableAdapter = new TPS_PAV.GUI.Reports.DataSetReportesTableAdapters.DataTable2TableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSetReportes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable2BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSetProgreso";
+            reportDataSource1.Value = this.DataTable1BindingSource;
+            reportDataSource2.Name = "DataSetFecha";
+            reportDataSource2.Value = this.DataTable2BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "TPS_PAV.GUI.Reports.ReportesCursos.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // DataSetReportes
+            // 
+            this.DataSetReportes.DataSetName = "DataSetReportes";
+            this.DataSetReportes.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DataTable1BindingSource
+            // 
+            this.DataTable1BindingSource.DataMember = "DataTable1";
+            this.DataTable1BindingSource.DataSource = this.DataSetReportes;
+            // 
+            // DataTable1TableAdapter
+            // 
+            this.DataTable1TableAdapter.ClearBeforeFill = true;
+            // 
+            // DataTable2BindingSource
+            // 
+            this.DataTable2BindingSource.DataMember = "DataTable2";
+            this.DataTable2BindingSource.DataSource = this.DataSetReportes;
+            // 
+            // DataTable2TableAdapter
+            // 
+            this.DataTable2TableAdapter.ClearBeforeFill = true;
+            // 
+            // FormReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "FormReporte";
+            this.Text = "FormReporte";
+            this.Load += new System.EventHandler(this.FormReporte_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataSetReportes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable2BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource DataTable1BindingSource;
+        private DataSetReportes DataSetReportes;
+        private System.Windows.Forms.BindingSource DataTable2BindingSource;
+        private DataSetReportesTableAdapters.DataTable1TableAdapter DataTable1TableAdapter;
+        private DataSetReportesTableAdapters.DataTable2TableAdapter DataTable2TableAdapter;
+    }
+}
